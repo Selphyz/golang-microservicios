1d15a9fe66c7619792959c906207f320f15641ff
Carlos <carlos.dom13@hotmail.com>
2021-06-13 05:35:30 +0200

    fix env variables

--- bookstore/db/mysql/users_db/users_db.go
+++ bookstore/db/mysql/users_db/users_db.go
@@ -8,23 +8,20 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-const (
-	mysql_username = "root"
-	mysql_password = "rootroot"
-	mysql_host = "127.0.0.1:3306"
-	mysql_schema = "users_db"
-)
-
-var (
+var(
 	DBClient *sql.DB
-	username = os.Getenv(mysql_username)
-	password = os.Getenv(mysql_password)
-	host = os.Getenv(mysql_host)
-	schema = os.Getenv(mysql_schema)
 )
-
 func init()  {
+	os.Setenv("mysql_username", "root")
+	os.Setenv("mysql_password", "rootroot")
+	os.Setenv("mysql_host", "127.0.0.1:3306")
+	os.Setenv("mysql_schema", "users_db")
+	var (
+		username 	= os.Getenv("mysql_username")
+		password 	= os.Getenv("mysql_password")
+		host 		= os.Getenv("mysql_host")
+		schema 		= os.Getenv("mysql_schema")
+	)
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
 		username, password, host, schema,
 	)
